646aa00da04f856be3152d7c3f8cf98a5ad8f781
dakgul <dakgul@gmail.com>
2019-07-10 23:39:42 +0200

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by apple on 10.07.2019.
+//  Copyright © 2019 Deniz Akgul. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
